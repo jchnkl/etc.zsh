8a214ce992945f9b80f7e41999dc4622b46b1f9d
Jochen Keil <jochen.keil@gmail.com>
2012-05-25 23:33:48 +0200

    Massive rewrite of zsh prompt configuration
    Improved overall design
    Less variable magic
    Less spaghetti (as good as it gets with shell code..)
    
    Wasted the better part of two days of my life on this

--- .zsh/50_prompt.zsh
+++ .zsh/50_prompt.zsh
@@ -1,15 +1,13 @@
-# global variables: pelems _LONGWEATHER _SHRTWEATHER _SHOMEP _LHOMEP
-#
 # TODO
 # updateWeather: check age of file and (don't) update
-# vcs_info: (g)it, (h)g, (s)vn -> red: unstaged, yellow: uncommited, green: ok
-# g:(collapsed_git_root)/path/within/git
-# if right prompt > (columns - leftprompt), collapse:
-# remove weather, date, etc, then collapse directory
-# if dir too long put it in line above
-#
 
-PERIOD=600
+local PERIOD=600
+local OLDCOLS=0
+local WEATHEROK=0
+local BATOK=0
+# 1.618 ^= golden ratio
+local _RPMAX=$(((${COLUMNS}*1000)/1618))
+
 
 local _NCOLO="%14F" # 66
 local _SCOLO="%59F" # 237 # 23 # 240
@@ -22,154 +20,346 @@ local _COMMA=",";
 local _LBRKT="[";
 local _RBRKT="]";
 local _HOSTSEP="@";
-local _PCHAR="%(!.#.$)";
-local _HIST="!%h";
 local _PWD="%~";
 local _LOGIN="%n";
 local _HOST="%2m";
-local _TIME="%D{%H:%M}";
-local _LONGDATE="%D{%A, %d. %B %Y}";
-local _SHRTDATE="%D{%a, %d. %b %y}";
-# local _VCS="${vcs_info_msg_0_}";
 
+typeset -A sprompt
+sprompt+=( " "     "${_WS}"                     )
+sprompt+=( " | "   "${_WS}${_SEP}${_WS}"        )
+sprompt+=( ","      "${_COMMA}"                 )
+sprompt+=( "["      "${_LBRKT}"                 )
+sprompt+=( "]"      "${_RBRKT}"                 )
+sprompt+=( "@"      "${_HOSTSEP}"               )
+sprompt+=( "#"      "%(!.#.$)"                  )
+sprompt+=( "pwd"    "${_PWD}"                   )
+sprompt+=( "name"   "${_LOGIN}"                 )
+sprompt+=( "host"   "${_HOST}"                  )
+sprompt+=( "time"   "%D{%H:%M}"                 )
+sprompt+=( "ldate"  "%D{%A}, %D{%d}%D{. %B %Y}" )
+sprompt+=( "sdate"  "%D{%a}, %D{%d}%D{. %b %y}" )
 
-# global array
-typeset -A pelems
-pelems+=(${_WS}		"${_WS}")
-pelems+=(${_SEP}	"${_SCOLO}${_SEP}%f")
-pelems+=(${_COMMA}	"${_SCOLO}${_COMMA}%f")
-pelems+=(${_LBRKT}	"%0K${_SCOLO}${_LBRKT}%f")
-pelems+=(${_RBRKT}	"${_SCOLO}${_RBRKT}%f%k")
-pelems+=(${_HOSTSEP}	"${_SCOLO}${_HOSTSEP}%f")
-pelems+=(${_PCHAR}	"%(!.%1F#%f.%4F$%f)")
-pelems+=(${_HIST}	"${_NCOLO}!${_EMPH}%h%f")
-pelems+=(${_PWD}	"%4F${_PWD}%f")
-pelems+=(${_LOGIN}	"${_ECOLO}${_LOGIN}%f")
-pelems+=(${_HOST}	"${_ECOLO}${_HOST}%f")
-pelems+=(${_TIME}	"${_EMPH}%D{%H:%M}%f")
-pelems+=(${_LONGDATE}	"${_EMPH}%D{%A}%f, ${_EMPH}%D{%d}%f${_NCOLO}%D{. %B %Y}")
-pelems+=(${_SHRTDATE}	"${_EMPH}%D{%a}%f, ${_EMPH}%D{%d}%f${_NCOLO}%D{. %b %y}")
-# pelems+=(${_VCS}	"${vcs_info_msg_0_}")
+typeset -A cprompt
+cprompt+=( " "     "${_WS}"                                                  )
+cprompt+=( " | "    "${_SCOLO}${_WS}${_SEP}${_WS}%f"                         )
+cprompt+=( ","      "${_SCOLO}${_COMMA}%f"                                   )
+cprompt+=( "["      "%0K${_SCOLO}${_LBRKT}%f"                                )
+cprompt+=( "]"      "${_SCOLO}${_RBRKT}%f%k"                                 )
+cprompt+=( "@"      "${_SCOLO}${_HOSTSEP}%f"                                 )
+cprompt+=( "#"      "%(!.%1F#%f.%4F$%f)"                                     )
+cprompt+=( "pwd"    "%4F${_PWD}%f"                                           )
+cprompt+=( "name"   "${_ECOLO}${_LOGIN}%f"                                   )
+cprompt+=( "host"   "${_ECOLO}${_HOST}%f"                                    )
+cprompt+=( "time"   "${_EMPH}%D{%H:%M}%f"                                    )
+cprompt+=( "ldate"  "${_EMPH}%D{%A}%f, ${_EMPH}%D{%d}%f${_NCOLO}%D{. %B %Y}" )
+cprompt+=( "sdate"  "${_EMPH}%D{%a}%f, ${_EMPH}%D{%d}%f${_NCOLO}%D{. %b %y}" )
 
+function mkHistPrompt () {
+    local HEVENTS=${(l.3..0.)$((${HISTCMD} % 10000))}
+    cprompt+=( "!" "${_NCOLO}!${_EMPH}${HEVENTS}%f" )
+    sprompt+=( "!" "!${HEVENTS}" )
+}
 
+function constructPrompt () {
 
+    local _elems= _prompt=
+    typeset -A _elems
+    _elems=(${(Pkv)1})
+    shift
 
-    fi
+    for e in $@; do
+        _prompt+=${_elems[$e]}
+    done
 
+    echo ${_prompt}
 
+}
 
+function mkNormalPrompt () {
+    local _lvl=$1; shift
 
+    typeset -a _prmpt
 
-
-
-
-
-
-
-        done
-
-        done
-
-        done
-
-
-
-
-
-
-
-
-
-
-        else
+    function dirPrompt () {
+        if [ ! ${PWD} = ${HOME} ]; then
+            resp=("pwd" " | ")
+            echo ${(pj:\0:)resp}
         fi
     }
 
-
-
-
-
-
-
-
-
-function updatePrompt() {
-    # global variables
-    _SHOMEP= _LHOMEP=
-    local _PSEP=${_WS}'\0'${_SEP}'\0'${_WS}
-    if [ -z "${SSH_CONNECTION}" ]; then
-        concatWith ${_PSEP} ${_LONGWEATHER} ${_LONGDATE} ${_TIME} ${_PBATTERY}; _LHOMEP+=(${reply})
-        concatWith ${_PSEP} ${_PWD} ${_SHRTWEATHER} ${_SHRTDATE} ${_TIME} ${_PBATTERY}; _SHOMEP+=(${reply})
-    else # prompt for ssh login
-        local _LGNATHST=${_LOGIN}'\0'${_HOSTSEP}'\0'${_HOST}
-        concatWith ${_PSEP} ${_LGNATHST} ${_LONGDATE} ${_TIME}; _LHOMEP+=(${reply})
-        concatWith ${_PSEP} ${_PWD} ${_LGNATHST} ${_SHRTDATE} ${_TIME}; _SHOMEP+=(${reply})
-    fi
-}
-
-
-# splitting of arguments is based on '\0'
-function concatWith() {
-    local _concatWithSep=${1}; shift;
-
-    reply=(${(s:\0:)1}); shift;
-    while [ -n "${1}" ]; do
-        reply+=(${(s:\0:)_concatWithSep} ${(s:\0:)1}); shift;
-    done
-}
-
-
-function buildPrompt() {
-    vcs_info
-
-    # if vcs -> vcs/path anstatt wetter/zeit
-
-    local _RP
-    _RP=(${_LBRKT})
-
-    if [ "${PWD}" = "${HOME}" ]; then
-        _RP+=(${_LHOMEP})
-    else
-        _RP+=(${_SHOMEP})
-    fi
-
-    _RP+=(${_RBRKT})
-
-    RPROMPT=
-    for e in ${_RP}; do
-        if ${_FANCY}; then
-            RPROMPT+=${(v)pelems[${e}]}
-        else
-            RPROMPT+=${(k)pelems[${e}]}
+    function weatherPrompt () {
+        if [ ${WEATHEROK} -eq 1 ]; then
+            resp=("$1" " | ")
+            echo ${(pj:\0:)resp}
         fi
-    done
+    }
+
+    function batPrompt () {
+        if [ ${BATOK} -eq 1 ]; then
+            resp=(" | " "$1")
+            echo ${(pj:\0:)resp}
+        fi
+    }
+
+    case ${_lvl} in
+        0)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "${(ps:\0:)$(weatherPrompt "lweather")}" \
+                     "ldate" " | " "time" \
+                     "${(ps:\0:)$(batPrompt "lbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        1)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "${(ps:\0:)$(weatherPrompt "sweather")}" \
+                     "ldate" " | " "time" \
+                     "${(ps:\0:)$(batPrompt "lbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        2)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "${(ps:\0:)$(weatherPrompt "sweather")}" \
+                     "sdate" " | " "time" \
+                     "${(ps:\0:)$(batPrompt "lbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        3)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "sdate" " | " "time" \
+                     "${(ps:\0:)$(batPrompt "lbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        4)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "sdate" " | " "time" \
+                     "${(ps:\0:)$(batPrompt "sbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        5)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "time" \
+                     "${(ps:\0:)$(batPrompt "sbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        6)
+            _prmpt=( "[" "pwd" \
+                     "${(ps:\0:)$(batPrompt "sbat")}" \
+                     "]" \
+                   )
+        ;;
+
+        7)
+            _prmpt=( "[" "pwd" "]" )
+        ;;
+
+        8)
+            _prmpt=( "[" "tpwd" "]" )
+        ;;
+        *) echo ;;
+    esac
+
+    constructPrompt $1 ${(v)_prmpt}
 
 }
 
+function mkSSHPrompt () {
+    local _lvl=$1; shift
 
-precmd_functions+=(updateBattery updatePrompt buildPrompt)
-periodic_functions+=(updateWeather)
+    typeset -a _prmpt
 
+    function dirPrompt () {
+        if [ ! ${PWD} = ${HOME} ]; then
+            resp=("pwd" " | ")
+            echo ${(pj:\0:)resp}
+        fi
+    }
+
+    case ${_lvl} in
+        0)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "name" "@" "host" " | " "ldate" " | " "time" \
+                     "]" \
+                   )
+        ;;
+
+        1)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "name" "@" "host" " | " "sdate" " | " "time" \
+                     "]" \
+                   )
+        ;;
+
+        2)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "name" "@" "host" " | " "time" \
+                     "]" \
+                   )
+        ;;
+
+        3)
+            _prmpt=( "[" "${(ps:\0:)$(dirPrompt)}" \
+                     "name" "@" "host" \
+                     "]" \
+                   )
+
+        ;;
+
+        4)
+            _prmpt=( "[" "pwd" "]" )
+
+        ;;
+
+        5)
+            _prmpt=( "[" "tpwd" "]" )
+
+        ;;
+        *) echo ;;
+    esac
+
+    constructPrompt $1 ${(v)_prmpt}
+}
+
+function mkRPrompt () {
+    if [ -z "${SSH_CONNECTION}" ]; then
+        mkNormalPrompt $@
+    else
+        mkSSHPrompt $@
+    fi
+}
+
+function mkTruncatedRPrompt () {
+
+    local _pelems=$1 _c=0 _tmp=
+
+    function tmpprompt () { mkRPrompt ${_c} sprompt }
+
+    _tmp=$(tmpprompt ${_c})
+
+    while [ ${#${(%)_tmp}} -gt ${_RPMAX} ]; do
+        _c=$((${_c} + 1))
+        _tmp=$(tmpprompt ${_c})
+    done
+
+    mkRPrompt ${_c} $_pelems
+
+}
+
+function updateRPrompt () {
+
+    local _TRUNLEN=$((${_RPMAX}/2 - 1))
+    local _CURPATH=${(%)${:-%~}}
+
+    sprompt+=( "tpwd" "%${_TRUNLEN}>».>%${_CURPATH}%>>%${_TRUNLEN}<.»<%${_CURPATH}%<<" )
+    cprompt+=( "tpwd" "%4F%${_TRUNLEN}>».>%${_CURPATH}%>>%${_TRUNLEN}<.«<%${_CURPATH}%<<%f" )
+
+    RPROMPT=$(mkTruncatedRPrompt ${_pelems} ${_RPMAX} )
+
+}
+
+function updatePrompt () {
+
+    if [ ${OLDCOLS} -ne ${COLUMNS} ]; then
+        OLDCOLS=${COLUMNS}
+        _RPMAX=$(((${COLUMNS}*1000)/1618))
+        for f in ${resize_functions}; do
+            eval $f
+        done
+    fi
+
+    PROMPT="
+$(constructPrompt ${_pelems} "[" "!" " " "#" "]" " " )"
+
+}
+
+periodicUpdateWeather () {
+    local shrtcolor="$(updateWeather "scolor"      )"
+    local shrtnocol="$(updateWeather "snocolor"    )"
+    local longcolor="$(updateWeather "lcolor"      )"
+    local longnocol="$(updateWeather "lnocolor"    )"
+
+    if [ -n "${shrtcolor}" -a -n "${shrtnocol}" -a -n "${longcolor}" -a -n "${longnocol}" ]; then
+        cprompt+=( "sweather" "${shrtcolor}" )
+        sprompt+=( "sweather" "${shrtnocol}" )
+        cprompt+=( "lweather" "${longcolor}" )
+        sprompt+=( "lweather" "${longnocol}" )
+        WEATHEROK=1
+    else
+        WEATHEROK=0
+    fi
+}
+
+preexecVCSUpdate () {
+
+    local sblen= trunc=
+    if [[ $1 =~ '^git.*' || $2 =~ '^git.*' || $# -eq 0 ]]; then
+
+        updateVCSPrompt
+        vcs_info
+
+        if [ -n "${vcs_info_msg_0_}" ]; then
+
+            sblen=$(((${_RPMAX}-${#vcs_info_msg_3_})/2 - 1))
+
+            if [ ${#vcs_info_msg_2_} -ge ${sblen} ]; then
+                trunc="%${sblen}>».>${vcs_info_msg_2_}%>>%${sblen}<.«<${vcs_info_msg_2_}%<<"
+            else
+                trunc=${vcs_info_msg_2_}
+            fi
+
+            sprompt+=( "pwd" "${vcs_info_msg_0_}${trunc}"      )
+            cprompt+=( "pwd" "${vcs_info_msg_1_}%4F${trunc}%f" )
+
+        fi
+
+    fi
+
+}
+
+function batteryUpdate () {
+    local _bprompt=
+    typeset -a _bprompt
+    _bprompt=( ${(ps:\0:)"$(updateBattery)"} )
+
+    if [ -n "${_bprompt}" ]; then
+        BATOK=1;
+
+        sprompt+=( "sbat" "${_bprompt[1]}" )
+        cprompt+=( "sbat" "${_bprompt[2]}" )
+        sprompt+=( "lbat" "${_bprompt[3]}" )
+        cprompt+=( "lbat" "${_bprompt[4]}" )
+
+    else
+        BATOK=0;
+    fi
+}
 
 autoload zsh/terminfo;
-if [[ "$terminfo[colors]" -ge 8 ]]; then _FANCY=true; else _FANCY=false; fi
 
+if [[ "$terminfo[colors]" -ge 8 ]]; then
+    _pelems=cprompt
+else
+    _pelems=sprompt
+fi
 
-#if [ -z "${_SHRTWEATHER}" -o -z "${_LONGWEATHER}" -o -z "${_PBATTERY}" ]; then
-    #updateWeather
-    #updateBattery
-    #updatePrompt
-#fi
+# initial update
+batteryUpdate
+preexecVCSUpdate
+periodicUpdateWeather
 
-
-local _BOTPROMPT=
-for e in ${_LBRKT} ${_HIST} ${_WS} ${_PCHAR} ${_RBRKT} ${_WS}; do
-    if ${_FANCY}; then
-        _BOTPROMPT+=${(v)pelems[${e}]}
-    else
-        _BOTPROMPT+=${(k)pelems[${e}]}
-    fi
-done
-
-PROMPT="
-${_BOTPROMPT}"
+chpwd_functions+=(preexecVCSUpdate updateRPrompt)
+resize_functions+=(preexecVCSUpdate updateRPrompt)
+precmd_functions+=(mkHistPrompt batteryUpdate updatePrompt)
+preexec_functions+=(preexecVCSUpdate updateRPrompt)
+periodic_functions+=(updateWeather updateRPrompt)
