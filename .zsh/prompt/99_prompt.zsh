f9e8b0fe35f6881dc42e600395e75012a89adb14
Jochen Keil <jochen.keil@gmail.com>
2012-05-29 12:26:45 +0200

    prefer the vcs prompt over the ssh prompt

--- .zsh/prompt/99_prompt.zsh
+++ .zsh/prompt/99_prompt.zsh
@@ -3,12 +3,12 @@ function setPrompts () {
 PROMPT="
 $( buildPrompt "[" "!" " " "#" "]" " " )"
 
-    if [[ -n "${SSH_CONNECTION}" ]] {
+    if [[ -n "${vcs_info_msg_0_}" ]] {
+        RPROMPT=$( vcsPrompt )
+    } elif [[ -n "${SSH_CONNECTION}" ]] {
         RPROMPT=$( sshPrompt )
     } elif [[ ${PWD} = ${HOME} ]] {
         RPROMPT=$( homePrompt )
-    } elif [[ -n "${vcs_info_message_0_}" ]] {
-        RPROMPT=$( vcsPrompt )
     } else {
         RPROMPT=$( pwdPrompt )
     }
