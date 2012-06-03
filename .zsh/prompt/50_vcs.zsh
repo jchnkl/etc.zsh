b1a0de840b45b525129c62d8f56fc0d1a473a1f1
Jochen Keil <jochen.keil@gmail.com>
2012-06-03 11:27:44 +0200

    rewrote the complete module
    make use of buildPrompt
    use generalized truncatePath
    use colorFunctions
    
    dynamic resizing works now for the vcs string itself
    system became more configurable

--- .zsh/prompt/50_vcs.zsh
+++ .zsh/prompt/50_vcs.zsh
@@ -1,25 +1,45 @@
 autoload -Uz vcs_info
 
 zstyle ':vcs_info::*' enable git # svn darcs hg
-zstyle ':vcs_info:*' max-exports 4
+zstyle ':vcs_info:*' max-exports 5
 zstyle ':vcs_info:*' stagedstr "%3F"
 zstyle ':vcs_info:*' unstagedstr "%9F"
+zstyle ':vcs_info:*' formats "%b" "%r" "%s" "%S" "%c%u"
 zstyle ':vcs_info:*' check-for-changes true
 
 zstyle ':vcs_info:git+pre-get-data:*:*' hooks updateVCSPrompt
 zstyle ':vcs_info:*+*:*' debug false
 
+
+local _vcs_dot="." _vcs_sep=":"
+
+plainElements+=( "vcs_dot" ${_vcs_dot} )
+plainElements+=( "vcs_sep" ${_vcs_sep} )
+
+elementSizes+=( "vcs_dot" ${#_vcs_dot} )
+elementSizes+=( "vcs_sep" ${#_vcs_sep} )
+
+colors+=( "vcs_dot"    $_dirc )
+colors+=( "vcs_sep"    $_tone )
+colors+=( "vcs_subdir" $_dirc )
+
+function _vcs_dirty_color  () { echo "%9F${1}%f"                   }
+function _vcs_clean_color  () { echo "%${_dirc}F${1}%f"            }
+function _vcs_branch_color () { echo "%2F${vcs_info_msg_4_}${1}%f" }
+
+colorFunctions+=( "vcs_branch"       _vcs_branch_color )
+
+local px= p0= p1= p2= p3= p4=
+typeset -a pX p0 p1 p2 p3 p4
+px=( "vcs_branch" "vcs_sep" )
+p0=( $px "vcs_long_repo"  "vcs_dot" "vcs_long_vcs"  )
+p1=( $px "vcs_long_repo"  "vcs_dot" "vcs_short_vcs" )
+p2=( $px "vcs_short_repo" "vcs_dot" "vcs_short_vcs" )
+p3=(     "vcs_short_repo" "vcs_dot" "vcs_short_vcs" )
+p4=( )
+
+
 +vi-updateVCSPrompt () {
-
-    local                 _cgit_prompt="%2F%c%u%b%f%59F:%4F%r.%1>>%s%>>"
-    local           _cgit_prompt_ahead="%2F%c%u%b%f%59F:%4F%r.%9F%1>>%s%>>"
-    local       _cgit_prompt_untracked="%2F%c%u%b%f%59F:%9F%r%4F.%1>>%s%>>"
-    local _cgit_prompt_ahead_untracked="%2F%c%u%b%f%59F:%9F%r%4F.%9F%1>>%s%>>"
-
-    local                 _sgit_prompt="%b:%r.%1>>%s%>>"
-    local           _sgit_prompt_ahead="%b:%r.%1>>%s%>>!"
-    local       _sgit_prompt_untracked="%b:%r?.%1>>%s%>>"
-    local _sgit_prompt_ahead_untracked="%b:%r?.%1>>%s%>>!"
 
     local _git_status="$(git status --short --branch 2>/dev/null)"
 
@@ -33,58 +53,110 @@ zstyle ':vcs_info:*+*:*' debug false
                               echo $? \
                             )
 
-    if [ ${_git_notAhead} -eq 1 -a ${_git_noUntracked} -eq 1 ]; then
-        _formats=( "${_sgit_prompt}" "${_cgit_prompt}" )
-    elif [ ${_git_notAhead} -eq 0 -a ${_git_noUntracked} -eq 1 ]; then
-        _formats=( "${_sgit_prompt_ahead}" "${_cgit_prompt_ahead}" )
-    elif [ ${_git_notAhead} -eq 1 -a ${_git_noUntracked} -eq 0 ]; then
-        _formats=( "${_sgit_prompt_untracked}" "${_cgit_prompt_untracked}" )
-    else
-        _formats=( "${_sgit_prompt_ahead_untracked}" \
-                   "${_cgit_prompt_ahead_untracked}" \
-                 )
-    fi
+    colorFunctions+=( "vcs_long_repo"    _vcs_clean_color  )
+    colorFunctions+=( "vcs_long_vcs"     _vcs_clean_color  )
+    colorFunctions+=( "vcs_short_repo"   _vcs_clean_color  )
+    colorFunctions+=( "vcs_short_vcs"    _vcs_clean_color  )
 
-    zstyle ':vcs_info:*' formats ${(@)_formats} "%S" "%b:%r.x:"
-    zstyle ':vcs_info:*' actionformats ${(@)_formats} "%S" "%b:%r.x:"
+    if [ ${_git_notAhead} -eq 0 -a ${_git_noUntracked} -eq 1 ]; then
+        colorFunctions+=( "vcs_long_vcs"     _vcs_dirty_color  )
+        colorFunctions+=( "vcs_short_vcs"    _vcs_dirty_color  )
+    elif [ ${_git_notAhead} -eq 1 -a ${_git_noUntracked} -eq 0 ]; then
+        colorFunctions+=( "vcs_long_repo"    _vcs_dirty_color  )
+        colorFunctions+=( "vcs_short_repo"   _vcs_dirty_color  )
+    elif [ ${_git_notAhead} -eq 0 -a ${_git_noUntracked} -eq 0 ]; then
+        colorFunctions+=( "vcs_long_repo"    _vcs_dirty_color  )
+        colorFunctions+=( "vcs_long_vcs"     _vcs_dirty_color  )
+        colorFunctions+=( "vcs_short_repo"   _vcs_dirty_color  )
+        colorFunctions+=( "vcs_short_vcs"    _vcs_dirty_color  )
+    fi
 
 }
 
-function updateVcsPrompt () {
-
-    trunc=
-    local sblen=
-    local maxlen=$( maxLen )
+updateVcsPrompt () {
 
     vcs_info
 
-    if [[ -n "${vcs_info_msg_0_}" ]] {
+    if [[ -z ${vcs_info_msg_0_} ]] {
+        return
+    }
 
-        sblen=$((${maxlen} - ${#vcs_info_msg_3_}))
-        sbtrunlen=$((${sblen}/2))
+    local trunpathlen= maxlen=$( maxLen ) ellipsis=${plainElements[ellipsis]}
 
-        if [ ${#vcs_info_msg_2_} -le ${sblen} ]; then
-            trunc="${vcs_info_msg_2_}"
-        elif [ ${sblen} -gt 8 -a ${#vcs_info_msg_2_} -ge ${sblen} ]; then
-            trunc="%${sbtrunlen}>>${vcs_info_msg_2_}%>>%${sbtrunlen}<${plainElements[ellipsis]}<${vcs_info_msg_2_}%<<"
-        else
-            trunc=""
-        fi
+    local _vcs_prompt= _vcs_prompt_len= _vcs_prompt_maxlen=
 
-        local vcs="${vcs_info_msg_0_}:${trunc}"
+    local _vcs_branch=${vcs_info_msg_0_}                         \
+          _vcs_long_repo=${vcs_info_msg_1_}                      \
+          _vcs_short_repo="%8>${ellipsis}>${vcs_info_msg_1_}%>>" \
+          _vcs_long_vcs="${vcs_info_msg_2_}"                     \
+          _vcs_short_vcs="%1>>${vcs_info_msg_2_}%>>"
 
-        function vcsColor () {
-            echo "${vcs_info_msg_1_}%59F:%f%${_dirc}F${trunc}%f"
+    plainElements+=( "vcs_branch"       ${_vcs_branch}        )
+    plainElements+=( "vcs_long_repo"    ${_vcs_long_repo}     )
+    plainElements+=( "vcs_long_vcs"     ${_vcs_long_vcs}      )
+    plainElements+=( "vcs_short_repo"   ${(%)_vcs_short_repo} )
+    plainElements+=( "vcs_short_vcs"    ${(%)_vcs_short_vcs}  )
+
+    elementSizes+=( "vcs_branch"       ${#_vcs_branch}           )
+    elementSizes+=( "vcs_long_repo"    ${#_vcs_long_repo}        )
+    elementSizes+=( "vcs_long_vcs"     ${#_vcs_long_vcs}         )
+    elementSizes+=( "vcs_short_repo"   ${#${(%)_vcs_short_repo}} )
+    elementSizes+=( "vcs_short_vcs"    ${#${(%)_vcs_short_vcs}}  )
+
+    if [[ ${#vcs_info_msg_3} -ge $(( ${maxlen} / 2 )) ]] {
+        _vcs_prompt_maxlen=$(( ${maxlen} / 2 ))
+    } else {
+        _vcs_prompt_maxlen=$(( ${maxlen} - ${#vcs_info_msg_3_} )) # - 3 ))
+    }
+
+    for p in p0 p1 p2 p3; {
+        _vcs_prompt_len=$(( $( calculateSize ${(@P)p} ) ))
+        if [[ ${_vcs_prompt_len} -le $(( ${_vcs_prompt_maxlen} )) ]] {
+            _vcs_prompt=$( buildPrompt ${(@P)p} )
+            break
         }
+    }
 
-        plainElements+=(  "vcs" ${vcs}        )
-        elementSizes+=(   "vcs" ${#${(%)vcs}} )
-        colorFunctions+=( "vcs" vcsColor      )
+    if [[ ${#_vcs_prompt} -le 2 ]] {
+        trunpathlen=$(( ${maxlen} - 2 ))
+    } else {
+        trunpathlen=$((${maxlen} - ${_vcs_prompt_len} - 3))
+    }
+
+    local truncatepath=$( truncatePath                              \
+                            ${trunpathlen}                          \
+                            ${plainElements[ellipsis]}              \
+                            ${vcs_info_msg_3_}                      \
+                        )
+
+    if [[ ${#truncatepath} -le 2 ]] {
+
+        plainElements+=( "vcs" ${_vcs_prompt} )
+        elementSizes+=(  "vcs" $(( ${_vcs_prompt_len} )) )
+
+    } elif [[ ${#_vcs_prompt} -le 2 ]] {
+
+        plainElements+=( "vcs_subdir" ${(%)truncatepath}     )
+        elementSizes+=(  "vcs_subdir" ${#${(%)truncatepath}} )
+
+        plainElements+=( "vcs" $( buildPrompt vcs_subdir ) )
+        elementSizes+=(  "vcs" ${elementSizes[vcs_subdir]} )
 
     } else {
-        plainElements+=(  "vcs" "" )
-        elementSizes+=(   "vcs" 0  )
-        colorFunctions+=( "vcs" "" )
+
+        plainElements+=( "vcs_subdir" ${(%)truncatepath}     )
+        elementSizes+=(  "vcs_subdir" ${#${(%)truncatepath}} )
+
+        plainElements+=( "vcs"                                             \
+                         ${_vcs_prompt}$( buildPrompt vcs_sep vcs_subdir ) \
+                       )
+
+        elementSizes+=( "vcs" $(( ${_vcs_prompt_len}          \
+                                + ${elementSizes[vcs_sep]}    \
+                                + ${elementSizes[vcs_subdir]} \
+                                )) \
+                      )
+
     }
 
 }
